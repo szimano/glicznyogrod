65150906d4226e3629a99c45ed04eb8f556ee82b
szimano <szimano@szimano.org>
2017-03-09 20:30:31 +0100

    Just a .gitignore fix

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start() {
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void QuitRequest() {
+		Debug.Log("Goodbye!");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
