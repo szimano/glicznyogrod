d58aa98078208673cee9b086cbe8de8510ef23f4
szimano <szimano@szimano.org>
2017-04-10 21:20:57 +0200

    ruchy i triggery

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1f, 1.5f)]
+	public float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);	
+	}
+
+	void OnTriggerEnter2D ()
+    {
+        Debug.Log("Got collision: " + name);
+    }
+
+    public void SetSpeed(float speed) {
+    	currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget(float damage) {
+    	Debug.Log(name + " caused damage: " + damage);
+    }
+}
